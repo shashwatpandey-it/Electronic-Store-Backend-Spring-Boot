815622b839cf631a4c858850ba058da1d0fc0dca
Shashwat Pandey <2001shashwat.rdr@gmail.com>
2023-03-30 22:29:25 +0530

    initial commit on master branch

---
+++ src/main/java/com/shashwat/electronicstorebackend/ElectronicStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shashwat.electronicstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashwat/electronicstorebackend/ElectronicStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashwat.electronicstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
